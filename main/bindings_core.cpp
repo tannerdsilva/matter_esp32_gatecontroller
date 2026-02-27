23a2320ef597d0f0fb6ad80a1e015b26f5a70d38
tannerdsilva <t@tannersilva.com>
2026-02-27 17:55:16 -0600

    wip

--- main/bindings_core.cpp
+++ main/bindings_core.cpp
@@ -1,5 +1,7 @@
 #include "bindings_core.h"
 #include <unordered_map>
+#include <mutex>
+#include <map>
 #include <esp_log.h>
 #include <esp_matter_client.h>
 #include <app/ReadClient.h>
@@ -25,11 +27,62 @@ class SubscriptionManager {
 			static SubscriptionManager instance;
 			return instance;
 		}
-		// esp_err_t AddBinding(const chip::)
+		esp_err_t AddBinding(const chip::app::Clusters::Binding::TableEntry &entry);
+		void RemoveBinding(const chip::app::Clusters::Binding::TableEntry &entry);
+		Subscription *Find(const chip::app::Clusters::Binding::TableEntry &key);
+	private:
+		SubscriptionManager() = default;
+		~SubscriptionManager() = default;
+		SubscriptionManager(const SubscriptionManager &) = delete;
+		SubscriptionManager &operator=(const SubscriptionManager &) = delete;
+
+		std::mutex m_mutex;
+
+		std::map<uint64_t, std::unique_ptr<Subscription>> m_subs;
+};
+
+class BooleanStateSubscriptionCallback : public chip::app::ReadClient::Callback {
+	public:
+		void OnReportBegin() override {
+			ESP_LOGI(TAG, "Report Begin");
+		}
+		void OnReportEnd() override {
+			ESP_LOGI(TAG, "Report End");
+		}
+
+		void OnSubscriptionEstablished(chip::SubscriptionId aSubscriptionId) override {
+			ESP_LOGE(TAG, "SUBSCRIPTION ESTABLISHED");
+		}
+
+		void OnAttributeData(const chip::app::ConcreteDataAttributePath &aPath, chip::TLV::TLVReader *aReader, const chip::app::StatusIB &aStatus) override {
+			// Handle the attribute data
+			ESP_LOGE(TAG, "\tATTRIBUTE DATA RECEIVED");
+			if (aPath.mClusterId == chip::app::Clusters::BooleanState::Id) {
+				ESP_LOGE(TAG, "\t\tATTRIBUTE DATA FOR BOOLEAN STATE CLUSTER");
+				if (aPath.mAttributeId == chip::app::Clusters::BooleanState::Attributes::StateValue::Id) {
+					ESP_LOGE(TAG, "\t\t\tReceived BooleanState attribute");
+				}
+			}
+		}
+
+		void OnEventData(const chip::app::EventHeader &aEventHeader, chip::TLV::TLVReader *apData, const chip::app::StatusIB *aStatus) override {
+			ESP_LOGE(TAG, "EVENT DATA!");
+		}
+
+		void OnError(CHIP_ERROR aError) override {
+			// Handle the error
+			ESP_LOGE(TAG, "ReadClient Error: %s", ErrorStr(aError));
+		}
+
+		void OnDone(chip::app::ReadClient * apReadClient) override {
+			// Cleanup after done
+			ESP_LOGE(TAG, "ReadClient Done");
+		}
 };
 
 void handle_binding_changed_event() {
 	ESP_LOGI(TAG, "BINDING TABLE CHANGED");
+	chip::app::Clusters::Binding::TableEntry binding_table;
 	for (auto &binding : chip::app::Clusters::Binding::Table::GetInstance()) {
 
 	}
